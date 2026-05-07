0baabaa9eb7d7894fbf27fbe9ae45dcbd2a7d0e7
Doarte45 <andresdornelas@gmail.com>
2026-05-06 22:34:10 -0500

    Added stuff for deployment on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://doarte45.github.io',
+  base: '/mysite',
   vite: {
     plugins: [tailwindcss()]
   }
